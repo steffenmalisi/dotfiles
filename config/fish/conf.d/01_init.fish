608077ee886d0eb15bd14b6a965f8b8e35197340
steffenmalisi <steffenmalisi@users.noreply.github.com>
2021-03-08 08:53:42 +0100

    Add mysql-client to path

--- config/fish/conf.d/01_init.fish
+++ config/fish/conf.d/01_init.fish
@@ -3,6 +3,7 @@ set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx XDG_CONFIG_HOME ~/.config
 
 set PATH $DOTFILES_DIR/bin $PATH
+set PATH /usr/local/opt/mysql-client/bin $PATH
 
 for dir in $DOTFILES_DIR/bin/extra/*
   if test -d $dir
