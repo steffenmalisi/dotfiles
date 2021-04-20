5ee4e9a5112a3cbcfee08a5636f99438c654536e
steffenmalisi <steffenmalisi@users.noreply.github.com>
2021-04-20 07:52:05 +0200

    Use a current version of openssl and curl

--- config/fish/conf.d/01_init.fish
+++ config/fish/conf.d/01_init.fish
@@ -4,6 +4,8 @@ set -gx XDG_CONFIG_HOME ~/.config
 
 set PATH $DOTFILES_DIR/bin $PATH
 set PATH /usr/local/opt/mysql-client/bin $PATH
+set PATH /usr/local/opt/openssl@1.1/bin $PATH
+set PATH /usr/local/opt/curl/bin $PATH
 
 for dir in $DOTFILES_DIR/bin/extra/*
   if test -d $dir
