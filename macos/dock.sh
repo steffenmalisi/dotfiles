d186e46230ca4cdf0592fb8c938c74f77bea94c1
steffenmalisi <steffenmalisi@users.noreply.github.com>
2025-02-05 10:01:20 +0100

    Reorganize dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,14 +3,12 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Applications/Chromium.app"
-dockutil --no-restart --add "/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/Postman.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-
-dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
 dockutil --no-restart --add "/Applications/Microsoft Teams.app"
-dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
+dockutil --no-restart --add "/Applications/Brave Browser.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/KeePassXC.app"
 
 
 killall Dock
