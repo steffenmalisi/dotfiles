a2bb5c8d23a948a95a4a703104ec17e8a7df6c96
steffenmalisi <steffenmalisi@users.noreply.github.com>
2025-02-05 10:36:43 +0100

    Use the right name for system settings

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/System Settings.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Microsoft Teams.app"
